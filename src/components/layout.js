bc7ba8ac10d25065e0fc75301d31f5b6f0adbfc9
shagia <usenet@shagia.me>
2019-07-05 13:52:39 -0700

    edit layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,1 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
+
+
+export default ({ children }) => (
+	<div>
+		<Link>
+			<h3>
+				Test Title
+			</h3>
+		</Link>
+		<Link
+			to={`/about/`}
+			css={css`float: right;`}>
+			About
+		</Link>
+		<br/>
+		{children}
+	</div>
+	)
